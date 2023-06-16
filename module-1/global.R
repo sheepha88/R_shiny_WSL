48036b48851462cfda3a91ebae8e090948bbcbe7
sheepha88 <jeong5726@gmail.com>
2023-06-16 17:21:19 +0900

    2023-06-16

--- module-1/global.R
+++ module-1/global.R
@@ -4,3 +4,27 @@
 library(shinyjs)
 library(shiny)
 library(glue)
+library(RMySQL)
+library(DBI)
+# library(jsonlite)
+# library(rjson)
+
+# ------------------------------------------------------------------------------------------------ #
+# mysql connect                                                                                    #
+# ------------------------------------------------------------------------------------------------ #
+
+# RMySQL connect --------------------------------------------------------------------------------- #
+con <- dbConnect(
+  RMySQL::MySQL(), 
+  user="root", 
+  password="0000",
+  host="127.0.0.1", 
+  dbname = "what_to_eat",
+  client.flag=CLIENT_MULTI_RESULTS
+)
+
+# utf control ------------------------------------------------------------------------------------ #
+dbSendQuery(con, "SET NAMES utf8;") 
+dbSendQuery(con, "SET CHARACTER SET utf8;") 
+dbSendQuery(con, "SET character_set_connection=utf8;")
+
